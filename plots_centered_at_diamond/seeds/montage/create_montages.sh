4b7de16368213678284a3b5392ff7739c317dfdc
Badel2 <2badel2@gmail.com>
2021-07-20 22:58:47 +0200

    Add plots per seed of interesting blocks like clay

--- plots_centered_at_diamond/seeds/montage/create_montages.sh
+++ plots_centered_at_diamond/seeds/montage/create_montages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 montage ../*/ores/coal*.png -geometry +2+2 -tile 7x6 montage_coal.png
 montage ../*/ores/copper*.png -geometry +2+2 -tile 7x6 montage_copper.png
 montage ../*/ores/diamond*.png -geometry +2+2 -tile 7x6 montage_diamonds.png
@@ -7,4 +8,16 @@ montage ../*/ores/gold*.png -geometry +2+2 -tile 7x6 montage_gold.png
 montage ../*/ores/iron*.png -geometry +2+2 -tile 7x6 montage_iron.png
 montage ../*/ores/lapis*.png -geometry +2+2 -tile 7x6 montage_lapis.png
 montage ../*/ores/redstone*.png -geometry +2+2 -tile 7x6 montage_redstone.png
-rm -i "*-1.png"
+
+montage ../*/minecraft:andesite_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_andesite.png
+montage ../*/minecraft:clay_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_clay.png
+montage ../*/minecraft:deepslate_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_deepslate.png
+montage ../*/minecraft:diorite_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_diorite.png
+montage ../*/minecraft:granite_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_granite.png
+montage ../*/minecraft:infested_deepslate_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_infested_deepslate.png
+montage ../*/minecraft:sand_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_sand.png
+montage ../*/minecraft:spawner_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_spawner.png
+montage ../*/minecraft:stone_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_stone.png
+montage ../*/minecraft:tuff_subchunk_freq.png -geometry +2+2 -tile 7x6 montage_tuff.png
+
+rm *-1.png
